6b68e021e599c0cb17d727c2da80d53f26566af5
Julio Ortiz <jcortizojeda@gmail.com>
2016-10-25 20:03:35 -0500

    estudiando angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.styl']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  public titulo:string;
+
+  
+  constructor(){
+   this.titulo = "LISTADO DE PELICULAS";
+  }
+  
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PeliculasListComponent } from './peliculas-list/peliculas-list.component';
+import { PeliculasFooterComponent } from './peliculas-footer/peliculas-footer.component';
+import { PeliculasFavoriteComponent } from './peliculas-favorite/peliculas-favorite.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasListComponent,
+    PeliculasFooterComponent,
+    PeliculasFavoriteComponent
   ],
   imports: [
     BrowserModule,
